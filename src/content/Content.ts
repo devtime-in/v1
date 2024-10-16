c2403bea6b73b6bfa48d3ee2e3911a348a348910
Romik Makavana <makavanaromik1214@gmail.com>
2024-10-16 12:47:42 +0530

    add routing

--- src/content/Content.ts
+++ src/content/Content.ts
@@ -31,48 +31,48 @@ import tiptap from "@/assets/svg/tiptap-icon.png";
 import redux from "@/assets/svg/redux-icon.svg";
 
 export const content = {
-    general: {
-        companyName: "Dev Time Technologies",
-        companyTextLogo: textLogo.src,
-        message: "Message",
-        email: "Email",
-        submit: "Submit",
-    },
-    header: {
-        navigationButtons: [
-            {
-                name: "Home",
-                href: "",
-            },
-            {
-                name: "About",
-                href: "",
-            },
-            {
-                name: "Services",
-                href: "",
-            },
-            {
-                name: "Projects",
-                href: "",
-            },
-            {
-                name: "Contact",
-                href: "",
-            },
-        ]
-    },
-    home: {
-        heading: 'The DevTime technologies',
-        subHeading: ["Mobile App development", "Front End development", "Full Stack development"],
-        banner: companyBanner.src
-    },
-    about: {
-        heading: 'About Us',
-        detils: [
-            {
-                title: 'A bit about me',
-                content: `
+  general: {
+    companyName: "DevTime Technologies",
+    companyTextLogo: textLogo.src,
+    message: "Message",
+    email: "Email",
+    submit: "Submit",
+  },
+  header: {
+    navigationButtons: [
+      {
+        name: "Home",
+        href: "",
+      },
+      {
+        name: "About",
+        href: "#about",
+      },
+      {
+        name: "Services",
+        href: "#services",
+      },
+      {
+        name: "Reviews",
+        href: "#reviews",
+      },
+      {
+        name: "Contact",
+        href: "#contact",
+      },
+    ]
+  },
+  home: {
+    heading: 'The DevTime technologies',
+    subHeading: ["Mobile App development", "Front End development", "Full Stack development"],
+    banner: companyBanner.src
+  },
+  about: {
+    heading: 'About Us',
+    detils: [
+      {
+        title: 'A bit about me',
+        content: `
                 I'm a self-taught web developer and Mobile App Developer with experience in designing
                 new features from ideation to production, implementation of wireframes and design
                 flows into high-performance software applications. I take into consideration the user
@@ -80,181 +80,181 @@ export const content = {
                 design, technology, and innovation in all my projects, which I like to accompany from
                 the first idea to release. Currently, I'm focused on the backend development.
               `,
-                image: about1.src
-            },
-            {
-                title: 'Technologies and Tools',
-                content: `
+        image: about1.src
+      },
+      {
+        title: 'Technologies and Tools',
+        content: `
                 Using a combination of cutting-edge technologies and reliable open-source software I
                 build user-focused, performant apps and websites for smartphones, tablets, and
                 desktops.
               `,
-                image: about2.src
-            },
-        ],
-        languages: [
-            {
-              name: 'HTML 5',
-              icon: html5.src,
-            },
-            {
-              name: 'CSS',
-              icon: css.src,
-            },
-            {
-              name: 'JavaScript',
-              icon: javascript.src,
-            },
-            {
-              name: 'React js',
-              icon: reactjs.src,
-            },
-            {
-              name: 'Angular',
-              icon: angular.src,
-            },
-            {
-              name: 'TypeScript',
-              icon: typescript.src,
-            },
-            {
-              name: 'Bootstrap',
-              icon: bootstrap.src,
-            },
-            {
-              name: 'Tailwind',
-              icon: tailwindcss.src,
-            },
-            {
-              name: 'sass',
-              icon: sass.src,
-            },
-            {
-              name: 'jQuery',
-              icon: jquery.src,
-            },
-            {
-              name: 'Material UI',
-              icon: materialUi.src,
-            },
-            {
-              name: 'Node js',
-              icon: nodejs.src,
-            },
-            {
-              name: 'Express',
-              icon: express.src,
-            },
-            {
-              name: 'MySQL',
-              icon: mysql.src,
-            },
-            {
-              name: 'MongoDB',
-              icon: mongodb.src,
-            },
-            {
-              name: 'PHP',
-              icon: php.src,
-            },
-            {
-              name: 'Laravel',
-              icon: laravel.src,
-            },
-            {
-              name: 'PostgreSQL',
-              icon: postgresql.src,
-            },
-            {
-              name: 'Firebase',
-              icon: firebase.src,
-            },
-            {
-              name: 'AWS',
-              icon: aws.src,
-            },
-            {
-              name: 'Heroku',
-              icon: heroku.src,
-            },
-            {
-              name: 'Git',
-              icon: git.src,
-            },
-            {
-              name: 'Postman',
-              icon: postman.src,
-            },
-            {
-              name: 'Stripe',
-              icon: stripe.src,
-            },
-            {
-              name: 'PayPal',
-              icon: paypal.src,
-            },
-            {
-              name: 'Tiptap',
-              icon: tiptap.src,
-            },
-            {
-              name: 'Redux',
-              icon: redux.src,
-            },
-          ]
-    },
-    services: {
-      heading: 'Services',
-      services : [
-        {
-          step: '01',
-          name: 'Design',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-        {
-          step: '02',
-          name: 'Develop',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-        {
-          step: '03',
-          name: 'Maintain',
-          description:
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-        },
-      ]
-    },
-    projects: {
-      heading: 'Projects',
-      reviews: [
-        {
-          name: 'Luca M',
-          message:
-            '“Always the best hamburger place in Biella. In addition to the classic sandwiches, the special ones that alternate throughout the year are also very good, such as the Autumn Special (see photo) with Arnad lard, honey and walnuts. Possibility to accompany everything with different local craft beers.”',
-        },
-        {
-          name: 'Nicole C',
-          message:
-            '“Always a pleasure to return as a fixed stop every time I return to Biella. Superb quality of ingredients and selection of local beers.”',
-        },
-        {
-          name: 'Lynn Lynn',
-          message:
-            '“Chanced upon this place due to a short rain. Was super impressed by the food , caffe latte and very goo service ! Will be back again !”',
-        },
-      ]
-    },
-    contact: {
-      heading: 'Get in Touch',
-      subHeading: "We are always happy to hear from our customers. Please feel free to contact us with any questions, comments, or concerns.",
-      address: '501, Pride Sapphire, Main Road, near Amin Marg, Vidhyakunj Society, Sirvar Park, Kotecha Nagar, Rajkot, Gujarat 360001',
-      phone: '+91 9913357614',
-      email: 'romikmakavana1214@gmail.com'
-    },
-    footer: {
-      heading: 'Footer',
-      discription: "We work to offer you an always fresh and excellent product using quality raw materials carefully selected from local suppliers."
-    }
+        image: about2.src
+      },
+    ],
+    languages: [
+      {
+        name: 'HTML 5',
+        icon: html5.src,
+      },
+      {
+        name: 'CSS',
+        icon: css.src,
+      },
+      {
+        name: 'JavaScript',
+        icon: javascript.src,
+      },
+      {
+        name: 'React js',
+        icon: reactjs.src,
+      },
+      {
+        name: 'Angular',
+        icon: angular.src,
+      },
+      {
+        name: 'TypeScript',
+        icon: typescript.src,
+      },
+      {
+        name: 'Bootstrap',
+        icon: bootstrap.src,
+      },
+      {
+        name: 'Tailwind',
+        icon: tailwindcss.src,
+      },
+      {
+        name: 'sass',
+        icon: sass.src,
+      },
+      {
+        name: 'jQuery',
+        icon: jquery.src,
+      },
+      {
+        name: 'Material UI',
+        icon: materialUi.src,
+      },
+      {
+        name: 'Node js',
+        icon: nodejs.src,
+      },
+      {
+        name: 'Express',
+        icon: express.src,
+      },
+      {
+        name: 'MySQL',
+        icon: mysql.src,
+      },
+      {
+        name: 'MongoDB',
+        icon: mongodb.src,
+      },
+      {
+        name: 'PHP',
+        icon: php.src,
+      },
+      {
+        name: 'Laravel',
+        icon: laravel.src,
+      },
+      {
+        name: 'PostgreSQL',
+        icon: postgresql.src,
+      },
+      {
+        name: 'Firebase',
+        icon: firebase.src,
+      },
+      {
+        name: 'AWS',
+        icon: aws.src,
+      },
+      {
+        name: 'Heroku',
+        icon: heroku.src,
+      },
+      {
+        name: 'Git',
+        icon: git.src,
+      },
+      {
+        name: 'Postman',
+        icon: postman.src,
+      },
+      {
+        name: 'Stripe',
+        icon: stripe.src,
+      },
+      {
+        name: 'PayPal',
+        icon: paypal.src,
+      },
+      {
+        name: 'Tiptap',
+        icon: tiptap.src,
+      },
+      {
+        name: 'Redux',
+        icon: redux.src,
+      },
+    ]
+  },
+  services: {
+    heading: 'Services',
+    services: [
+      {
+        step: '01',
+        name: 'Design',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      },
+      {
+        step: '02',
+        name: 'Develop',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      },
+      {
+        step: '03',
+        name: 'Maintain',
+        description:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+      },
+    ]
+  },
+  projects: {
+    heading: 'Reviews',
+    reviews: [
+      {
+        name: 'Luca M',
+        message:
+          '“Always the best hamburger place in Biella. In addition to the classic sandwiches, the special ones that alternate throughout the year are also very good, such as the Autumn Special (see photo) with Arnad lard, honey and walnuts. Possibility to accompany everything with different local craft beers.”',
+      },
+      {
+        name: 'Nicole C',
+        message:
+          '“Always a pleasure to return as a fixed stop every time I return to Biella. Superb quality of ingredients and selection of local beers.”',
+      },
+      {
+        name: 'Lynn Lynn',
+        message:
+          '“Chanced upon this place due to a short rain. Was super impressed by the food , caffe latte and very goo service ! Will be back again !”',
+      },
+    ]
+  },
+  contact: {
+    heading: 'Get in Touch',
+    subHeading: "We are always happy to hear from our customers. Please feel free to contact us with any questions, comments, or concerns.",
+    address: '501, Pride Sapphire, Main Road, near Amin Marg, Vidhyakunj Society, Sirvar Park, Kotecha Nagar, Rajkot, Gujarat 360001',
+    phone: '+91 9913357614',
+    email: 'romikmakavana1214@gmail.com'
+  },
+  footer: {
+    heading: 'Footer',
+    discription: "We work to offer you an always fresh and excellent product using quality raw materials carefully selected from local suppliers."
+  }
 }
